28024393d3d955f17e3cf50fc3cfb8b9a3cee387
Ubuntu 64-bit 300Gb <souhail.philippon@gmail.com>
2019-08-02 17:00:40 +0200

    mysite (Djangotraining2.0)

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -1,6 +1,21 @@
 from django.contrib import admin
 
-from .models import Question, Choice
+from .models import Choice, Question
+
+
+# class QuestionAdmin(admin.ModelAdmin):
+#     fields = ['pub_date', 'question_text']
+#
+# admin.site.register(Question, QuestionAdmin)
+
+# class QuestionAdmin(admin.ModelAdmin):
+#     fieldsets = [
+#         (None,               {'fields': ['question_text']}),
+#         ('Date information', {'fields': ['pub_date']}),
+#     ]
+#
+# admin.site.register(Question, QuestionAdmin)
+# admin.site.register(Choice)
 
 
 class ChoiceInline(admin.TabularInline):
@@ -17,4 +32,5 @@ class QuestionAdmin(admin.ModelAdmin):
     list_display = ('question_text', 'pub_date', 'was_published_recently')
     list_filter = ['pub_date']
     search_fields = ['question_text']
+
 admin.site.register(Question, QuestionAdmin)
--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -1,8 +1,9 @@
 import datetime
 
 from django.db import models
-from django.urls import reverse
 from django.utils import timezone
+
+# Create your models here.
 
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
@@ -12,10 +13,11 @@ class Question(models.Model):
         return self.question_text
 
     def was_published_recently(self):
-        now = timezone.now()
-        return now - datetime.timedelta(days=1) <= self.pub_date <= now
+        # return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 
-    def was_published_recently(self):
+        # now = timezone.now()
+        # return now - datetime.timedelta(days=1) <= self.pub_date <= now
+
         now = timezone.now()
         return now - datetime.timedelta(days=1) <= self.pub_date <= now
 
@@ -27,5 +29,6 @@ class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
+
     def __str__(self):
         return self.choice_text
